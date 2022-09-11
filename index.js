4a7712b4f092b478ebe009b6151b25978a3799f7
repti85 <ids-reptile@bk.ru>
2022-09-11 03:34:33 +0300

    зrocessing request for date

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const convert = require("xml-js");
 const app = express();
 const PORT = process.env.port || 3003;
 
+let currencyDate;
+
 const yesterdayDate = () => {
-  const date = new Date();
+  const date = new Date(currencyDate.split(".").reverse().join(","));
   let dd = date.getDate() - 1;
   if (dd < 10) dd = "0" + dd;
 
@@ -15,7 +17,7 @@ const yesterdayDate = () => {
 
   let yy = date.getFullYear();
   if (yy < 10) mm = "0" + yy;
-  console.log(`${dd}/${mm}/${yy}`);
+  console.log(`yesterdayDate ${dd}/${mm}/${yy}`);
   return `${dd}/${mm}/${yy}`;
 };
 
@@ -23,21 +25,6 @@ const getCurrency = async () => {
   debugger;
   let ratesDataToday;
   let ratesDataYesterday;
-
-  await needle(
-    "get",
-    `http://www.cbr.ru/scripts/XML_daily.asp?date_req=${yesterdayDate()}`,
-    {
-      parse_response: false,
-    }
-  ).then((response) => {
-    response = convert.xml2js(response.body, {
-      compact: true,
-      spaces: 1,
-    });
-    ratesDataYesterday = response.ValCurs.Valute;
-  })
-  .catch((err) => console.log('get yesterday', err));
 
   await needle("get", "http://www.cbr.ru/scripts/XML_daily.asp", {
     parse_response: false,
@@ -47,15 +34,36 @@ const getCurrency = async () => {
         compact: true,
         spaces: 1,
       });
+      console.log(`response>>>>>>`, response);
       ratesDataToday = response.ValCurs.Valute;
+      currencyDate = response.ValCurs._attributes.Date;
+      console.log(`currencyDate`, currencyDate);
       console.log("ratesDataToday>>>>>>>>", ratesDataToday);
     })
-    .catch((err) => console.log('get today', err));
+    .catch((err) => console.log("get today", err));
 
-  const merge = [];
+  await needle(
+    "get",
+    `http://www.cbr.ru/scripts/XML_daily.asp?date_req=${yesterdayDate()}`,
+    {
+      parse_response: false,
+    }
+  )
+    .then((response) => {
+      response = convert.xml2js(response.body, {
+        compact: true,
+        spaces: 1,
+      });
+      ratesDataYesterday = response.ValCurs.Valute;
+      console.log("ratesDataYesterday>>>>>>>>", ratesDataYesterday);
+    })
+    .catch((err) => console.log("get yesterday", err));
 
-  for (let i = 0; i < ratesDataToday.length; i++) {  
-    merge.push({
+  const merge = [[]];
+  merge.unshift({ currencyDate });
+  for (let i = 0; i < ratesDataToday.length; i++) {
+    merge[1].push({
+      // currencyDate,
       currencyTicker: ratesDataToday[i].CharCode._text,
       currencyName: ratesDataToday[i].Name._text,
       currencyNominal: ratesDataToday[i].Nominal._text,
@@ -63,11 +71,13 @@ const getCurrency = async () => {
         Number(ratesDataToday[i].Value._text.replace(",", ".")).toFixed(2)
       ),
       currencyPriceYesterday: String(
-        Number(ratesDataYesterday[i].Value._text.replace(",", ".")).toFixed(2) 
+        Number(ratesDataYesterday[i].Value._text.replace(",", ".")).toFixed(2)
       ),
       difference: String(
-        (Number(ratesDataToday[i].Value._text.replace(",", ".")) -
-          Number(ratesDataYesterday[i].Value._text.replace(",", "."))).toFixed(2) 
+        (
+          Number(ratesDataToday[i].Value._text.replace(",", ".")) -
+          Number(ratesDataYesterday[i].Value._text.replace(",", "."))
+        ).toFixed(2)
       ),
     });
   }
@@ -83,48 +93,50 @@ const getCurrencyForDate = async (props) => {
     {
       parse_response: false,
     }
-  ).then((response) => {
-    response = convert.xml2js(response.body, {
-      compact: true,
-      spaces: 1,
-    });
-    currencyForDate = response.ValCurs.Valute;
-  })
-  .catch((err) => console.log('get yesterday', err));
+  )
+    .then((response) => {
+      response = convert.xml2js(response.body, {
+        compact: true,
+        spaces: 1,
+      });
+      currencyForDate = response.ValCurs.Valute;
+      currencyDate = response.ValCurs._attributes.Date;
+    })
+    .catch((err) => console.log("get yesterday", err));
 
-  const currencyForDateRebuild = [];
-
-  for (let i = 0; i < currencyForDate.length; i++) {  
-    currencyForDateRebuild.push({
+  const currencyForDateRebuild = [[]];
+  currencyForDateRebuild.unshift({ currencyDate })
+  for (let i = 0; i < currencyForDate.length; i++) {
+    currencyForDateRebuild[1].push({
       currencyTicker: currencyForDate[i].CharCode._text,
       currencyName: currencyForDate[i].Name._text,
       currencyNominal: currencyForDate[i].Nominal._text,
       currencyPriceToday: String(
-        Number(currencyForDate[i].Value._text.replace(",", ".")).toFixed(2)
+        Number(currencyForDate[i].Value._text.replace(",", ".")).toFixed(2) 
       ),
-      currencyPriceYesterday: '',
-      difference: '',
+      currencyPriceYesterday: "",
+      difference: "",
     });
   }
-  return currencyForDateRebuild;  
-}
+  return currencyForDateRebuild;
+};
 
 app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server starting on port ${PORT}`);  
+  console.log(`Server starting on port ${PORT}`);
 });
 
 app.get("/api", async (req, res) => {
   console.log("get request");
   const data = await getCurrency();
-  console.log("data >>>>>>>>>>> ", JSON.stringify(data)); 
+  console.log("data >>>>>>>>>>> ", JSON.stringify(data));
   res.json(data);
 });
 
 app.get("/date_request", async (req, res) => {
   console.log("get request CurrencyForDate", req.query.date);
   const data = await getCurrencyForDate(req.query.date);
-  console.log("data >>>>>>>>>>> ", JSON.stringify(data)); 
+  console.log("data >>>>>>>>>>> ", JSON.stringify(data));
   res.json(data);
 });
